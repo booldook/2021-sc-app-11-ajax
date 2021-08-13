7e43eae91b44907be59e020df7b601471dae24e2
booldook <booldook@gmail.com>
2021-08-13 10:22:00 +0900

    데이터 가져오기 성공

--- js/02.search.js
+++ js/02.search.js
@@ -1,13 +1,39 @@
 /*************** global init **************/
+var auth = 'KakaoAK f17d0ae4d1d2ec94f5d272fd59b55b7f';
 
 
 /************** user function *************/
+function getPath(cate) {
+	return 'https://dapi.kakao.com/'+(cate === 'book' ? 'v3' : 'v2')+'/search/'+cate;
+}
+
+function getParams(query) {
+	return {
+		params: { query: query },
+		headers: { Authorization: auth }
+	}
+}
 
 
 /************** event callback ************/
+function onSubmit(e) {
+	e.preventDefault();
+	var cate = $(this).find('select[name="category"]').val().trim();
+	var query = $(this).find('input[name="query"]').val().trim();
+	axios.get(getPath(cate), getParams(query)).then(onSuccess).catch(onError);
+}
+
+function onSuccess(res) {
+	console.log(res);
+}
+
+function onError(err) {
+	console.log(err);
+}
 
 
 /*************** event init ***************/
+$('.search-form').submit(onSubmit);
 
 
 /*************** start init ***************/
