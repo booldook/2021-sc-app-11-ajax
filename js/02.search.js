b106be7e90738a34266cc930fe5a0f90a4a982d4
booldook <booldook@gmail.com>
2021-08-17 11:50:57 +0900

    정리

--- js/02.search.js
+++ js/02.search.js
@@ -37,13 +37,13 @@ function setBlogLists(r) {
 	var html = '';
 	r.forEach(function(v, i) {
 		html  = '<li class="list">';
-		html += '<a class="thumbs" href="'+v.url+'">';
+		html += '<a class="thumbs" href="'+v.url+'" target="_blank">';
 		html += '<img src="'+v.thumbnail+'" alt="'+v.title+'" class="w100">';
 		html += '</a>';
 		html += '<div class="contents">';
-		html += '<a class="title" href="'+v.url+'">'+v.title+'</a>';
+		html += '<a class="title" href="'+v.url+'" target="_blank">'+v.title+'</a>';
 		html += '<p class="content">'+v.contents+'</p>';
-		html += '<a class="name" href="'+v.url+'">'+v.blogname+'</a> | <a href="'+v.url+'" class="link">'+v.url+'</a>';
+		html += '<a class="name" href="'+v.url+'" target="_blank">'+v.blogname+'</a> | <a href="'+v.url+'" class="link" target="_blank">'+v.url+'</a>';
 		html += '<div class="dt">'+moment(v.datetime).format('YYYY-MM-DD HH:mm:ss')+'</div>';
 		html += '</div>';
 		html += '</li>';
@@ -87,16 +87,16 @@ function setClipLists(r) {
 	var html = '';
 	r.forEach(function(v, i) {
 		html  = '<li class="list">';
-		html += '<a class="thumbs" href="'+v.url+'">';
+		html += '<a class="thumbs" href="'+v.url+'" target="_blank">';
 		html += ' <img src="'+v.thumbnail+'" alt="'+v.title+'" class="w100">';
 		html += '</a>';
 		html += '<div class="contents">';
-		html += ' <a class="title" href="'+v.url+'">'+v.title+'</a>';
+		html += ' <a class="title" href="'+v.url+'" target="_blank">'+v.title+'</a>';
 		html += ' <div>';
-		html += '  <a class="author" href="'+v.url+'">'+v.author+'</a> | ';
+		html += '  <a class="author" href="'+v.url+'" target="_blank">'+v.author+'</a> | ';
 		html += '  <span class="play-time">'+getPlayTime(v.play_time)+'</span>';
 		html += ' </div>';
-		html += ' <a href="'+v.url+'" class="link">'+v.url+'</a>';
+		html += ' <a href="'+v.url+'" class="link" target="_blank">'+v.url+'</a>';
 		html += ' <div class="dt">'+moment(v.datetime).format('YYYY-MM-DD HH:mm:ss')+'</div>';
 		html += '</div>';
 		html += '</li>';
--- js/util.js
+++ js/util.js
@@ -18,7 +18,9 @@ function numberFormat(v) {
 
 function getPlayTime(s) {
 	if(Math.floor(s/60) >= 60)
-		return Math.floor(s/3600) + ':' + Math.floor(s%3600) + ':' + zp(s%60);
+		return zp(Math.floor(s/3600)) + ':' + zp(Math.floor(s/60%60)) + ':' + zp(s%60);
 	else
 		return zp(Math.floor(s/60)) + ':' + zp(s%60); 
 }
+
+
