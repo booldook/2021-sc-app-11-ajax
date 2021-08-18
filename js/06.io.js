e2c80ab4f566a31f9a547f4c44a5db8b95bd3408
booldook <booldook@gmail.com>
2021-08-18 10:55:38 +0900

    06.io 완료

--- js/06.io.js
+++ js/06.io.js
@@ -14,14 +14,36 @@ function onFn(el, rootValue, option) {
 
 var observer;
 var option = {
-	root: null,
-	rootMargin: '0px',
+	// root: null,
+	// rootMargin: '0px',
+	// threshold: 0
+	// threshold: [0, .25, .5, .75, 1],
 }
 observer = new IntersectionObserver(onIntersection, option);
 observer.observe(document.querySelector('.wrapper'));
 
 function onIntersection(el, observer) {
-	console.log(el, observer);
+	el.forEach(function(v, i) {
+		if(v.isIntersecting) {
+			if(v.intersectionRatio < .25) {
+				$(v.target).css('background-color', 'red');
+			}
+			else if(v.intersectionRatio >= .25 && v.intersectionRatio < .5) {
+				$(v.target).css('background-color', 'green');
+			}
+			else if(v.intersectionRatio >= .5 && v.intersectionRatio < .75) {
+				$(v.target).css('background-color', 'blue');
+			}
+			else if(v.intersectionRatio >= .75) {
+				$(v.target).css('background-color', 'purple');
+			}
+		}
+		else {
+			$(v.target).css('background-color', 'beige');
+		}
+		console.log(v.isIntersecting);
+	});
+	// console.log(el, observer);
 }
 
 
